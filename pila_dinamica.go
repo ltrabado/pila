badb8bf22d6703d3024490c2efd0b8478dedd554
lt@genII <lautaro.trabado@hotmail.com>
2023-09-27 13:23:36 -0300

    Modelo de implementacion para primitivas CrearPilaDinamica, EstaVacia, VerTope y Desapilar.

--- pila_dinamica.go
+++ pila_dinamica.go
@@ -6,3 +6,42 @@ type pilaDinamica[T any] struct {
 	datos    []T
 	cantidad int
 }
+
+/* Constructor del TAD Pila. */
+
+func CrearPilaDinamica[T any]() Pila[T] {
+    pila := new(pilaDinamica[T]) // se agrega el generic
+    // hago lo que deba hacer
+    return pila
+}
+
+// EstaVacia devuelve verdadero si la pila no tiene elementos apilados, false en caso contrario.
+func EstaVacia() bool {
+	if cantidad == 0 {
+		return true
+	}
+	return false
+}
+
+// VerTope obtiene el valor del tope de la pila. Si la pila tiene elementos se devuelve el valor del tope.
+// Si está vacía, entra en pánico con un mensaje "La pila esta vacia".
+func VerTope() T {
+	if EstaVacia() == true {
+		panic("La pila esta vacia") 
+	}
+	return datos[cantidad - 1]
+}
+
+// Apilar agrega un nuevo elemento a la pila.
+Apilar(T)
+
+// Desapilar saca el elemento tope de la pila. Si la pila tiene elementos, se quita el tope de la pila, y
+// se devuelve ese valor. Si está vacía, entra en pánico con un mensaje "La pila esta vacia".
+func Desapilar() T {
+	if EstaVacia() == true {
+		panic("La pila esta vacia") 
+	}
+	elementoDesapilado := datos[cantidad - 1]
+	cantidad--
+	return elementoDesapilado
+}
