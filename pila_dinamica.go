6823d4caccc013b4ade78a48bd6d85853fad4fe4
lt@genII <lautaro.trabado@hotmail.com>
2023-10-03 23:25:51 -0300

    Funciones pasan primera etapa de pruebas sin redimension.

--- pila_dinamica.go
+++ pila_dinamica.go
@@ -11,14 +11,16 @@ func CrearPilaDinamica[T any]() Pila[T] {
 	const _CAPACIDADINICIAL = 10
     pila := new(pilaDinamica[T]) // se agrega el generic
     // hago lo que deba hacer
-	pila.datos := make([]T, _CAPACIDADINICIAL)
-	pila.cantidad := 0
+	pila.datos = make([]T, _CAPACIDADINICIAL)
+	pila.cantidad = 0
     return pila
 }
 
+
+
 // EstaVacia devuelve verdadero si la pila no tiene elementos apilados, false en caso contrario.
-func EstaVacia() bool {
-	if cantidad == 0 {
+func (pila *pilaDinamica[T]) EstaVacia() bool {
+	if pila.cantidad == 0 {
 		return true
 	}
 	return false
@@ -26,17 +28,18 @@ func EstaVacia() bool {
 
 // VerTope obtiene el valor del tope de la pila. Si la pila tiene elementos se devuelve el valor del tope.
 // Si está vacía, entra en pánico con un mensaje "La pila esta vacia".
-func VerTope() T {
-	if EstaVacia() == true {
+func (pila *pilaDinamica[T]) VerTope() T {
+	if pila.EstaVacia() == true {
 		panic("La pila esta vacia") 
 	}
-	return datos[cantidad-1]
+	return pila.datos[pila.cantidad-1]
 }
 
 /* redimensionar ajusta la capacidad de la pila de la siguiente manera:
 	1) Si la pila se encuentra recien creada (pila vacia y capacidad igual a cero), se le asigna una capacidad inicial de 10.
 	2) Si cantidad es igual a la capacidad de la pila, se duplica su capacidad.
 	3) Si cantidad es menor o igual a la cuarta parte de la capacidad de la pila, se reduce su capacidad a la mitad. */
+/*
 func redimensionar() T {
 	if (EstaVacia() == (cap(datos)==0)) {
 		//Asignar capacidad = 10
@@ -47,24 +50,25 @@ func redimensionar() T {
 	if (cantidad*4) <= cap(datos) {
 		//capacidad=capacidad/2
 	}
-}
+}*/
 // Apilar agrega un nuevo elemento a la pila.
-func Apilar(T) {
-	if cantidad == cap(datos) {
+func (pila *pilaDinamica[T]) Apilar(elem T) {
+	if pila.cantidad == cap(pila.datos) {
 		//redimensionar
 	}
-	datos[cantidad] = T
-	cantidad++
+	pila.datos[pila.cantidad] = elem
+	pila.cantidad++
+	return
 }
 
 // Desapilar saca el elemento tope de la pila. Si la pila tiene elementos, se quita el tope de la pila, y
 // se devuelve ese valor. Si está vacía, entra en pánico con un mensaje "La pila esta vacia".
-func Desapilar() T {
-	if EstaVacia() == true {
+func (pila *pilaDinamica[T]) Desapilar() T {
+	if pila.EstaVacia() == true {
 		panic("La pila esta vacia") 
 	}
-	elementoDesapilado := datos[cantidad-1]
-	cantidad--
+	elementoDesapilado := pila.datos[pila.cantidad-1]
+	pila.cantidad--
 	//redimensionar
 	return elementoDesapilado
 }
